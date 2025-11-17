6b39b4908b16e6b211dcd111bd637cbc912fc500
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-11-16 19:01:32 -0800

    Refactor config for database path and secret key
    Updated the database URI to use the app's base directory and changed the secret key format.

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
